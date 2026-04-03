2de94ccd92d11067cf715ade6288e774c58b0b67
Aadhi vee <aadhivee@gmail.com>
2026-04-03 09:51:23 +0530

    add self ping keep alive

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,5 +1,20 @@
 from django.apps import AppConfig
+import threading
+import requests
+import time
 
+def keep_alive():
+    while True:
+        time.sleep(10 * 60)  # every 10 minutes
+        try:
+            requests.get("https://sun-backend2.onrender.com/health/")
+            print("✅ Self ping successful")
+        except Exception as e:
+            print(f"❌ Self ping failed: {e}")
 
 class AccountsConfig(AppConfig):
     name = 'accounts'
+
+    def ready(self):
+        t = threading.Thread(target=keep_alive, daemon=True)
+        t.start()
